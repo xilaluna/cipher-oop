4c8da16270efafb2712a7f225f59900cf8c9a5bc
xiluna <xilaluna2@gmail.com>
2020-12-03 14:19:06 -0800

    init

---
+++ UserMenu.py
@@ -0,0 +1,14 @@
+import cipher
+
+
+class UserMenu():
+    """
+    takes in user input
+    """
+
+    def __init__(self, cipher):
+        self.cipher = cipher
+
+    def user_chipher(self, cipher):
+
+        pass
---
+++ cipher.py
@@ -0,0 +1,6 @@
+from UserMenu import UserMenu
+
+user_cipher_choice = input(
+    'Which cipher would you like to use to decode/encrypt :')
+
+user = UserMenu(user_cipher_choice)
