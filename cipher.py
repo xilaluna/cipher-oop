826b237d6dad8cd80fa04a53cc0fb45a45773086
xiluna <xilaluna2@gmail.com>
2020-12-07 00:00:43 -0800

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,8 +1,6 @@
-from UserMenu import UserMenu
+import UserMenu
 
 user_cipher_choice = input(
     'Which cipher would you like to use to decode/encrypt :')
 
 user_cryptography = input('Which cipher would you like to choose')
-
-user = UserMenu(user_cipher_choice)
