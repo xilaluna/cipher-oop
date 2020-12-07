d944293e14e4a2b0cf8f76229c5d510400b6a75c
xiluna <xilaluna2@gmail.com>
2020-12-06 23:16:25 -0800

    Update Decryption.py

--- Decryption.py
+++ Decryption.py
@@ -9,18 +9,22 @@ class Decryption(Transform):
     Decrypts user message
     """
 
-    def __init__(self, message):
+    def __init__(self, message, cipher):
         self.message = message
+        self.cipher = cipher
 
     def base64_decrypt(self):
         base64_bytes = self.message.encode('ascii')
         message_bytes = base64.b64decode(base64_bytes)
         decoded_message = message_bytes.decode('ascii')
-        return decoded_message
+        print(decoded_message)
 
     def caesar_cipher_decrypt(self):
         for i in range(len(string.ascii_uppercase)):
             print(f'{i} | {Encryption.caesar_cipher_encrypt(self.message, i)}')
 
     def transform(self):
-        pass
+        if self.cipher == 'base64':
+            return self.base64_decrypt()
+        elif self.cipher == 'caesar cipher':
+            return self.caesar_cipher_decrypt()
