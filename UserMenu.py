31aa592696247b182bff05282b685b29f56e2931
xiluna <xilaluna2@gmail.com>
2020-12-08 11:05:02 -0800

    Update UserMenu.py

--- UserMenu.py
+++ UserMenu.py
@@ -8,15 +8,15 @@ class UserMenu():
     """
 
     def __init__(self, cryptography, cipher, message, shift_num='1'):
-        self.cryptography = cryptography
+        self._cryptography = cryptography
         self.cipher = cipher
         self.message = message
         self.shift_num = shift_num
 
     def cryptography_choice(self):
-        if self.cryptography == '1':
+        if self._cryptography == '1':
             encryption = Encryption(self.message)
             return encryption.transform(self.cipher, self.shift_num)
-        elif self.cryptography == '2':
+        elif self._cryptography == '2':
             decryption = Decryption(self.message)
             return decryption.transform(self.cipher)
